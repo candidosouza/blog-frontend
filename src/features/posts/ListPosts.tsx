4b01cfa1e7acf67a640742cbf4b3c208b1cebe49
candidosouza <candidosouzza@gmail.com>
2023-10-10 08:34:38 -0300

    feat: change layout

--- src/features/posts/ListPosts.tsx
+++ src/features/posts/ListPosts.tsx
@@ -8,16 +8,16 @@ export default function ListPosts() {
     const posts = useAppSelector(selectPosts);
 
     const postsList = posts.map((post) => (
-        <Grid item xs={12} md={6} key={post.id} sx={{mb: 4}}>
+        <Grid item xs={12} md={12} key={post.id} sx={{mb: 6}}>
             <Typography variant="caption" component="h2" sx={{ mb: 1 }}>
                 {post.category}
             </Typography>
-            <Typography variant="h4" color="text.secondary" sx={{ mb: 2 }}>
+            <Typography variant="h2" component="h1" color="text.secondary" sx={{ mb: 2 }}>
                 {post.title}
             </Typography>
 
             <Typography variant="body1" color="text.primary" sx={{ mb: 2 }}>{post.content}</Typography>
-            <Typography variant="caption" component="h2" sx={{ mb: 1 }}>
+            <Typography variant="caption" component="h3" sx={{ mb: 1 }}>
                 Autor: {post.author}
             </Typography>
             <Link 
